c274c8abd4b7e54f5aae43753e3e416a10cb3676
Joan Claverol <joanclaverol@gmail.com>
2019-08-10 20:23:18 +0200

    lecture of temerature copied

---
+++ read_dht11.py
@@ -0,0 +1,21 @@
+# https://github.com/szazo/DHT11_Python
+import RPi.GPIO as GPIO
+import dht11
+import time
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+instance = dht11.DHT11(pin = 4)
+result = instance.read()
+while True:
+    if result.is_valid():
+        print("Temperature: %-3.1f C" % result.temperature)
+        print("Humidity: %-3.1f %%" % result.humidity)
+    else:
+        print("Error: %d" % result.error_code)
+
+    time.sleep(1)
