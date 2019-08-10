f3e037b7fa763099b1b838a41c319966db553066
Joan Claverol <joanclaverol@gmail.com>
2019-08-10 20:07:49 +0200

    comments added to the script

--- INFOIN.py
+++ INFOIN.py
@@ -1,9 +1,9 @@
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4,GPIO.IN)
-try:
-    while True:
-        input_value = GPIO.input(4)
-        print(input_value)
+import RPi.GPIO as GPIO # import libraries
+GPIO.setmode(GPIO.BCM) # escull si el tipo de GPIO es board o BCM
+GPIO.setup(4,GPIO.IN) # assignan el GPIO 4 que sera d entrada
+try: # intenta la seguent accio
+    while True: # loop infinit
+        input_value = GPIO.input(4) # el port de entrada se't guarda  en el valor input
+        print(input_value) 
 finally:
-    GPIO.cleanup()
+    GPIO.cleanup() # si no funciona, stop el try
